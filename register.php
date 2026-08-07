c67ed9364167cf6128165e3864472b990b07e1a1
VINIX-10 <alvinnurfaiz01@gmail.com>
2026-08-07 14:53:14 +0700

    menambahkan peringatan

--- register.php
+++ register.php
@@ -1,4 +1,7 @@
-<?php require_once __DIR__ . '/config/ddos_layer.php'; ?>
+<?php require_once __DIR__ . '/config/ddos_layer.php'; 
+// 🚧⚠️ File Register ini masih dalam tahap pengembangan, jadi beberapa fitur mungkin belum sepenuhnya berfungsi.
+// jangan gunakan register.php dulu sampai comment ini terhapus 🚧🗼
+?>
 <!DOCTYPE html>
 <html lang="id">
 <head>
