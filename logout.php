773e82974b494b216b839781073ebe045fd5234c
VINIX-10 <alvinnurfaiz01@gmail.com>
2026-08-10 09:41:08 +0700

    meperbaiki route logout

--- logout.php
+++ logout.php
@@ -2,6 +2,6 @@
 session_start();
 session_unset();    // Hapus semua variabel session
 session_destroy();  // Hancurkan session
-header("Location: login.php"); // Lempar kembali ke halaman login
+header("Location: index.php"); // Lempar kembali ke halaman login
 exit;
 ?>
