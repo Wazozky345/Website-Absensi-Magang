fae240d202ff29cf4a533776fd5695e1e828f13c
VINIX-10 <alvinnurfaiz01@gmail.com>
2026-08-28 12:46:56 +0700

    memperbaiki sesi

--- proses/proses_mentor_tugas.php
+++ proses/proses_mentor_tugas.php
@@ -1,8 +1,5 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
+require_once __DIR__ . '/../config/sesi.php';
 require_once __DIR__ . '/../config/koneksi.php';
 
 // Validasi Hak Akses Mentor
